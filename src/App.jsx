954cdb890409f5f3aab1bc28b44b18f9d26d3066
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2026-04-19 20:59:50 +0530

    refactor: clean up import statements and remove unused state variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 class="text-3xl font-bold underline">sanjay this side</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
